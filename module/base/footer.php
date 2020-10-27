9e8a7278ed88ef259b9454af8d029647e2c555aa
VannotAlexi0x73 <alexis.vannot@outlook.fr>
2020-10-27 16:38:59 +0100

    [ADD] footer responsive

--- module/base/footer.php
+++ module/base/footer.php
@@ -3,7 +3,7 @@
         <div>
             <h4>Liens utiles</h4>
             <ul class="list_stylized">
-                <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Accueil</a></li>
+                <li><a href="/index.php"><i class="fa fa-angle-double-right"></i>Accueil</a></li>
                 <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>A propos</a></li>
                 <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Vidéos</a></li>
             </ul>
@@ -11,22 +11,21 @@
         <div>
             <h4>Réseaux sociaux</h4>
             <ul class="list_stylized">
-                <li><a href="javascript:void();"><i class="fab fa-youtube"></i>Youtube</a></li>
-                <li><a href="javascript:void();"><i class="fab fa-facebook"></i>Facebook</a></li>
-                <li><a href="javascript:void();"><i class="fab fa-instagram"></i>Instagram</a></li>
-                <li><a href="javascript:void();"><i class="fab fa-twitter"></i>Twitter</a></li>
+                <li><a href="https://www.youtube.com"><i class="fab fa-youtube"></i>Youtube</a></li>
+                <li><a href="https://www.instagram.com"><i class="fab fa-instagram"></i>Instagram</a></li>
+                <li><a href="https://www.twitter.com"><i class="fab fa-twitter"></i>Twitter</a></li>
             </ul>
         </div>
         <div>
             <h4>A propos</h4>
             <ul class="list_stylized">
-                <li><a href="/modules/login/contact_us.php"><i class="fa fa-angle-double-right"></i>Contactez-nous</a></li>
+                <li><a href="/module/login/contact_us.php"><i class="fa fa-angle-double-right"></i>Contactez-nous</a></li>
                 <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
                 <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Imprint</a></li>
             </ul>
         </div>
     </div>
     <div class="footer_copyright">
-        <p>&copy All right Reversed. DONZE Valentin & VANNOT Alexis</p>
+        &copy All right Reversed. DONZE Valentin & VANNOT Alexis
     </div>
 </footer>
