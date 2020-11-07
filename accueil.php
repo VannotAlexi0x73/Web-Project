4bafd9e8099f76dae752c1fad5b3f8647ec6e8e2
Valentin <valdonze26@gmail.com>
2020-11-07 16:18:30 +0100

    [Ref] carousel

--- accueil.php
+++ accueil.php
@@ -20,20 +20,29 @@
         <!-- Include header -->
         <?php include "module/base/header.php"; ?>
 
-        <div class="cadre">
-            <ul id="diapo" class="diaporama">
-                <li><img src="https://i1.sndcdn.com/artworks-000165384395-rhrjdn-t500x500.jpg" alt="Image 1"></li>
-                <li><img src="https://i1.sndcdn.com/artworks-000185743981-tuesoj-t500x500.jpg" alt="Image 1"></li>
-                <li><img src="https://i1.sndcdn.com/artworks-000158708482-k160g1-t500x500.jpg" alt="Image 1"></li>
-                <li><img src="https://i1.sndcdn.com/artworks-000062423439-lf7ll2-t500x500.jpg" alt="Image 1"></li>
-                <li><img src="https://i1.sndcdn.com/artworks-000028787381-1vad7y-t500x500.jpg" alt="Image 1"></li>
-                <li><img src="https://i1.sndcdn.com/artworks-000108468163-dp0b6y-t500x500.jpg" alt="Image 1"></li>
-                <li><img src="https://i1.sndcdn.com/artworks-000064920701-xrez5z-t500x500.jpg" alt="Image 1"></li>
-            </ul>
+        <div class="carousel">
+            <div class="images_carousel">
+                <img src="https://i1.sndcdn.com/artworks-000064920701-xrez5z-t500x500.jpg" id="lastClone" alt="Image 1">
+                <img src="https://i1.sndcdn.com/artworks-000165384395-rhrjdn-t500x500.jpg" alt="Image 1">
+                <img src="https://i1.sndcdn.com/artworks-000185743981-tuesoj-t500x500.jpg" alt="Image 2">
+                <img src="https://i1.sndcdn.com/artworks-000158708482-k160g1-t500x500.jpg" alt="Image 3">
+                <img src="https://i1.sndcdn.com/artworks-000062423439-lf7ll2-t500x500.jpg" alt="Image 4">
+                <img src="https://i1.sndcdn.com/artworks-000028787381-1vad7y-t500x500.jpg" alt="Image 5">
+                <img src="https://i1.sndcdn.com/artworks-000108468163-dp0b6y-t500x500.jpg" alt="Image 6">
+                <img src="https://i1.sndcdn.com/artworks-000064920701-xrez5z-t500x500.jpg" alt="Image 7">
+                <img src="https://i1.sndcdn.com/artworks-000165384395-rhrjdn-t500x500.jpg" id="firstClone" alt="Image 1">
+                
+            </div>
         </div>
+        <button id="prev_Button">Précédent</button>
+        <button id="next_Button">Suivant</button>
 
+        <script src="js/carousel.js"></script>
+        
         <!-- Include footer -->
         <?php include "module/base/footer.php"; ?>
     </body>
 
 </html>
+
+
