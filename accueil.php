c1b8e648bcb95ff22f7e70309a2c7dc32f11202c
VannotAlexi0x73 <alexis.vannot@outlook.fr>
2020-11-09 18:46:22 +0100

    [ADD] index for carousel

--- accueil.php
+++ accueil.php
@@ -30,30 +30,16 @@
             <i class="fas fa-angle-left" id="prev_Button"></i>
             <div class="images_carousel">
                 <?php $query = $db->query("SELECT name, image from `movie.showing`");
+                        $index = 0;
                     while ($movieShowing = $query->fetch()) {
-                        echo '<img src="data:image/jpg;base64,' . base64_encode($movieShowing['image']) . '"  alt="' . $movieShowing['name'] . '" title="' . $movieShowing['name'] . '"/>';
+                        echo '<img src="data:image/jpg;base64,' . base64_encode($movieShowing['image']) . '"  alt="' . $movieShowing['name'] . '" title="' . $movieShowing['name'] . '" id="' . $index . '"/>';
+                        $index++;
                 } ?>
             </div>
         </div>
 
-        <!-- <div class="carousel">
-            <i class="fas fa-angle-right" id="next_Button"></i>
-            <i class="fas fa-angle-left" id="prev_Button"></i>
-            <div class="images_carousel">
-                <img src="https://i1.sndcdn.com/artworks-000064920701-xrez5z-t500x500.jpg" id="lastClone" alt="7">
-                <img src="https://i1.sndcdn.com/artworks-000165384395-rhrjdn-t500x500.jpg" alt="1">
-                <img src="https://i1.sndcdn.com/artworks-000185743981-tuesoj-t500x500.jpg" alt="2">
-                <img src="https://i1.sndcdn.com/artworks-000158708482-k160g1-t500x500.jpg" alt="3">
-                <img src="https://i1.sndcdn.com/artworks-000062423439-lf7ll2-t500x500.jpg" alt="4">
-                <img src="https://i1.sndcdn.com/artworks-000028787381-1vad7y-t500x500.jpg" alt="5">
-                <img src="https://i1.sndcdn.com/artworks-000108468163-dp0b6y-t500x500.jpg" alt="6">
-                <img src="https://i1.sndcdn.com/artworks-000064920701-xrez5z-t500x500.jpg" alt="7">
-                <img src="https://i1.sndcdn.com/artworks-000165384395-rhrjdn-t500x500.jpg" id="firstClone" alt="1">
-            </div>
-        </div> -->
-
         <script src="js/carousel.js"></script>
-        
+
         <!-- Include footer -->
         <?php include "module/base/footer.php"; ?>
     </body>
